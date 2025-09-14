067b05d736ce7a58a13ec80a59e614316f9d314f
Ehab <2100632@eng.asu.edu.eg>
2025-09-14 17:19:19 +0300

    add all

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-
+using System;
 namespace TodoApi
 {
     public class Program
@@ -14,6 +16,23 @@ namespace TodoApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Add Azure App Configuration here, before Startup
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        var settings = config.Build();
+                        var endpoint = settings["AppConfig:Endpoint"];
+
+                        if (!string.IsNullOrEmpty(endpoint))
+                        {
+                            // Connect to Azure App Configuration using Managed Identity
+                            config.AddAzureAppConfiguration(options =>
+                                options.Connect(
+                                    new Uri(endpoint),
+                                    new ManagedIdentityCredential()));
+                        }
+                    });
+
+                    // This remains the same
                     webBuilder.UseStartup<Startup>();
                 });
     }
